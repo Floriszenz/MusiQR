63a269feac7dae18725c6cf673994a6bc48f43ac
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2022-12-16 13:56:45 +0100

    feat: implement landing page

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<button
+    on:click
+    class="bg-blue-500 text-white rounded-xl text-lg px-6 font-semibold py-3 hover:scale-105 transition-transform"
+>
+    <slot />
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Button from "$lib/components/Button.svelte";
+
+    function onScanClick() {}
+
+    function onUploadClick() {
+        console.log("Clicked on upload button");
+    }
+</script>
+
+<svelte:head>
+    <title>MusiQR - Turn QR codes into music 🎶</title>
+</svelte:head>
+
+<main
+    class="w-full h-screen flex flex-col gap-4 justify-center items-center bg-gradient-to-b from-slate-50 to-slate-400"
+>
+    <h1 class="text-4xl mb-8">MusiQR</h1>
+    <Button on:click={onScanClick}>Scan with camera</Button>
+    <Button on:click={onUploadClick}>Upload image</Button>
+</main>
