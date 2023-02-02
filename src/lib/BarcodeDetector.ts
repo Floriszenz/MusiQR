fc18619e6cf3b59a59ec9221ee230861d4152e4a
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2023-02-02 18:21:11 +0100

    wip: use fallback barcode detector always

--- src/lib/BarcodeDetector.ts
+++ src/lib/BarcodeDetector.ts
@@ -4,8 +4,8 @@ export type BarcodeDetector = BarcodeDetectorPolyfill;
 export type BarcodeDetectorConstructor = typeof BarcodeDetectorPolyfill;
 
 export async function initBarcodeDetector(): Promise<BarcodeDetectorConstructor> {
-    if ("BarcodeDetector" in window) {
-        return window.BarcodeDetector as unknown as BarcodeDetectorConstructor;
+    if (false && "BarcodeDetector" in window) {
+        // return window.BarcodeDetector as unknown as BarcodeDetectorConstructor;
     } else {
         const { BarcodeDetectorPolyfill } = await import("@undecaf/barcode-detector-polyfill");
 
